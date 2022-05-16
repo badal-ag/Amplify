b49c971a0787d0caacdeb580fc3154443b08dd4b
Badal Agarwal <badal.agarwal1369@gmail.com>
2022-05-16 13:53:51 +0530

    Version 2

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing AWS Amplify Auth, Storage and Database
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bit.ly/badal-agarwal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit the Website here
         </a>
       </header>
     </div>
